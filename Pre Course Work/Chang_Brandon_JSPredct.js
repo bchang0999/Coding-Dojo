6ee14134e74af4798acb8d385fdb068e92cf79f3
bchang0999 <bchang0999@gmail.com>
2022-04-18 17:13:16 -0400

    first commit

---
+++ Pre Course Work/Chang_Brandon_JSPredct.js
@@ -0,0 +1,25 @@
+function myBirthYearFunc(){
+    console.log("I was born in " + 1980);
+} 
+//"The console will log what year your born by using a variable to fill in the + x"
+
+
+
+
+function myBirthYearFunc(birthyearinput){
+    console.log("I was born in " + birthyearinput)
+}
+//"The console log will state "I was born in (birthyearinput)"
+//"Example would look like "I was born in 1997"
+
+
+
+function add(num1,num2){
+    console.log("summing numbers!");
+    console.log("num1 is: " + num1)
+    console.log("num2 is: " + num2)
+    var sum = num1 + num2;
+    console.log(sum);
+}
+//My prediction would be the console log would state
+// Summing Numbers! num1 is 10 num2 is 20 30
