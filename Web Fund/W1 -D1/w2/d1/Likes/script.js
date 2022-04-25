a1ff9a4fcf000e47bd41688fdc2f0731dbd45599
bchang0999 <bchang0999@gmail.com>
2022-04-25 14:40:46 -0400

    Like assignment

---
+++ Web Fund/W1 -D1/w2/d1/Likes/script.js
@@ -0,0 +1,31 @@
+var like = 9;
+var likeElement= document.querySelector("#like");
+
+console.log(likeElement);
+
+function add1(){
+    like++;
+    likeElement.innerHTML = like + " like(s)";
+    console.log(like);
+}
+
+var like2 = 9;
+var like2Element= document.querySelector("#like2");
+
+console.log(like2Element);
+
+function add2(){
+    like2++;
+    like2Element.innerHTML = like2 + " like(s)";
+    console.log(like2);
+}
+var like3 = 12;
+var like3Element= document.querySelector("#like3");
+
+console.log(like3Element);
+
+function add3(){
+    like3++;
+    like3Element.innerHTML = like3 + " like(s)";
+    console.log(like3);
+}
