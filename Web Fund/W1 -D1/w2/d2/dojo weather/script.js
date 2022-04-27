b21183d0a8743efa518e772aeaea7214e140f574
bchang0999 <bchang0999@gmail.com>
2022-04-27 11:53:23 -0400

    dojo weather

--- Web Fund/W1 -D1/w2/d2/dojo weather/script.js
+++ Web Fund/W1 -D1/w2/d2/dojo weather/script.js
@@ -4,8 +4,49 @@ function cookieMonster(){
 cookie.remove();
 }
 
-var chigh = document.querySelector('.bc-bottom p')
+var chigh = document.querySelectorAll('#high')
+var clow = document.querySelectorAll('#low')
 function heat(element){
-    chigh = chigh*2+30 
-    document.querySelector(".bc-bottom p").innerText = chigh
+    var select = document.querySelector('select')
+    var value = select.options[select.selectedIndex].value;
+    console.log(value)
+    console.log (element)
+    
+    if( value=="f"){
+
+        for( var i=0; i<chigh.length; i++){
+        x = parseInt(chigh[i].innerText[0]+chigh[i].innerText[1])
+        x = x*2+30 
+        chigh[i].innerHTML = x + "&#176;"
+        y = parseInt(clow[i].innerText[0]+clow[i].innerText[1])
+        y = y*2+30 
+        clow[i].innerHTML = y + "&#176;"
+        } 
+    
+    }else {
+        for( var i=0; i<chigh.length; i++){
+            x = parseInt(chigh[i].innerText[0]+chigh[i].innerText[1])
+            x = (y-30)/2  
+            chigh[i].innerHTML = x + "&#176;"
+            y = parseInt(clow[i].innerText[0]+clow[i].innerText[1])
+            y = (y-30)/2 
+            clow[i].innerHTML = y + "&#176;"
+            } 
+
+    }
+
+
+    
+
+    
+
+
+
+
+
+
+
+
+
 }
+
